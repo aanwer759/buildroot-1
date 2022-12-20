dc4a9e7e14d1bdf98f7cbd553bfba0e252586e14
Ali Anwer <anwer@crunch.es-lab.de>
2022-12-20 15:34:21 +0100

    build complete with output

--- package/hello/hello.mk
+++ package/hello/hello.mk
@@ -5,11 +5,11 @@
 ##################################################################
 
 HELLO_VERSION = 1.0
-HELLO_SITE = /package/hello/src
+HELLO_SITE = ./package/hello/src
 HELLO_SITE_METHOD = local
 
 define HELLO_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -c $(@D)
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
 endef
 
 define HELLO_INSTALL_TARGET_CMDS
